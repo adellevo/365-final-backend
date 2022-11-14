406b7eae1d61176ae2f0fedd403bc95dd1b4a67f
jack-kelly-22 <jacklaxjk@gmail.com>
2022-11-14 11:08:38 -0800

    add jwt implementation starter for endpoints with auth required use the @jwt_required directive

--- project/auth.py
+++ project/auth.py
@@ -1,20 +1,13 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_jwt_extended import create_access_token
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user
 from .models import User
 import jsonify
 from . import db
-# https://prod.liveshare.vsengsaas.visualstudio.com/join?7174E3856CBFA5BD076070B8632B16F383B0
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
-def login():
-    return render_template('login.html')
-
-@auth.route('/signup')
-def signup():
-    return render_template('signup.html')
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
@@ -28,18 +21,17 @@ def signup_post():
     user = User.query.filter_by(username=username).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Username already exists')
-        return redirect(url_for('auth.signup'))
+        return jsonify({"message": "Username already exists"}), 409
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     # new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
     new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
-
+    
     # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
-
-    return redirect(url_for('auth.login'))
+    new_user["auth_token"] = create_access_token(identity = data["username"])
+    return jsonify({"message": "User created successfully"}), 201
 
 @auth.route('/logout')
 @login_required
@@ -50,30 +42,14 @@ def logout():
 @auth.route('/login', methods=['POST'])
 def login_post():
     data = jsonify(request.json)
-
-    # login code goes here
     username = data["username"]
     password = data["password"]
-    print("passwordddddddd: ", password)
-
     remember = True if request.form.get('remember') else False
-
-    # user = User.query.filter_by(email=email).first()
     user = User.query.filter_by(username=username).first()
-
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
-        return None
-
-    return user
-    
-    # if not user or not check_password_hash(user.password, password):
-    #     flash('Please check your login details and try again.')
-    #     print(user.username)
-    #     print(check_password_hash(user.password, password))
-    #     return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
-
-    # if the above check passes, then we know the user has the right credentials
-    # login_user(user, remember=remember)
-    # return redirect(url_for('main.profile'))
+        return jsonify({"message": "Invalid username or password"}), 401
+    access_token = create_access_token(identity=username)
+    user["access_token"] = access_token
+    return jsonify(user)
--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
-
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 # create Flask instance
 app = Flask(__name__)
 CORS(app)
+# should load this from env
+app.config['SECRET_KEY'] = 'super-secret'
+
+jwt = JWTManager(app)
 
 @app.route("/")
 
