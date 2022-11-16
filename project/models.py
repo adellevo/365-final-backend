8b04c0140946fc03cb450ab815aced776cbdc7f7
Adelle Vo <adelle.vo@gmail.com>
2022-11-15 20:02:11 -0800

    new updates

--- project/models.py
+++ project/models.py
@@ -1,6 +1,8 @@
 from flask_login import UserMixin
 from . import db
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, Table
+# from flaskl_sqlalchemy import SQLAlchemy.
 # https://prod.liveshare.vsengsaas.visualstudio.com/join?7174E3856CBFA5BD076070B8632B16F383B0
 class User(db.Model):
     def get_id(self):
@@ -18,34 +20,37 @@ class User(db.Model):
 
         }
 
+class Stash(db.Model):
+    def get_id(self):
+           return (self.stashId)
 
-# class Transaction(db.Model):
-#        def get_id(self):
-#            return (self.TransactionId)
-
-#        transactionId = db.Column(db.Integer, primary_key=True) 
-#        address = db.Column(db.String(64))
-#        function = db.Column(db.String(100))
-#        date = db.Column(db.Integer)
-#        userId = db.Column(db.Integer)
-#        payloadId = db.Column(db.Integer)
-#        stashId = db.Column(db.Integer)
-#        userId = db.Column(db.Integer)
-
-# class Payload(db.Model):
-#        def get_id(self):
-#               return (self.PayloadId)
-       
+    stashId = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(64))
+    userId = db.Column(db.Integer,db.ForeignKey("user.userId"))
+    walletId = db.Column(db.Integer)
 
 
-# class Event(db.Model):
-#        def get_id(self):
-#               return (self.eventId)
-#        eventId = db.Column(db.Integer)
-#        name = db.Column(db.String)
-#        eventHandle = db.Column(db.String(64))
-#        transactionId = db.Column(db.Integer)
 
+class Transaction(db.Model):
+       def get_id(self):
+           return (self.transactionId)
+
+       transactionId = db.Column(db.Integer, primary_key=True) 
+       address = db.Column(db.String(64))
+       function = db.Column(db.String(100))
+    #    date = db.Column(db.String(100))
+       stashId = db.Column(db.Integer)
+    #    userId = db.Column(db.Integer,db.ForeignKey("user.userId"))
+    
+
+class Event(db.Model):
+       def get_id(self):
+              return (self.eventId)
+       eventId = db.Column(db.Integer, primary_key=True)
+       eventType = db.Column(db.String(50))
+       name = db.Column(db.String(50))
+       amount = db.Column(db.Integer,nullable=True)
+       transactionId = db.Column(db.Integer,db.ForeignKey("transaction.transactionId"))
 
 
 class Wallet(db.Model):
@@ -53,31 +58,16 @@ class Wallet(db.Model):
            return (self.walletId)
 
     walletId = db.Column(db.Integer, primary_key=True) 
+    userId = db.Column(db.Integer,db.ForeignKey("user.userId"))
     address = db.Column(db.String(50), unique=True)
-    privateKey = db.Column(db.String(50), unique=True)
-    userId = db.Column(db.Integer, unique=True)
+    privateKey = db.Column(db.String(50), unique=True,nullable=True)
 
-# table that maps users to wallets
-class UsersWallets(db.Model):
-    id = db.Column(db.Integer, primary_key=True) 
-    walletId = db.Column(db.Integer) 
-    userId = db.Column(db.Integer) 
-
-# class Stash(db.Model):
-#     def get_id(self):
-#            return (self.stashId)
-
-#     stashId = db.Column(db.Integer, primary_key=True) 
-#     privateKey = db.Column(db.String(64), unique=True)
-#     userId = db.Column(db.Integer, unique=True)
-#     walletId = db.Column(db.Integer, unique=True)
-
-# class Parameter(db.Model):
-#     def get_id(self):
-#            return (self.stashId)
-
-#     genericType = db.Column(db.String(50), unique=True)
-#     name = db.Column(db.String(50), unique=True)
-#     value = db.Column(db.String(50), unique=True)
-#     parameterId = db.Column(db.Integer, unique=True)
+class Arg(db.Model):
+    def get_id(self):
+           return (self.stashId)
+    argId = db.Column(db.Integer, primary_key=True, unique=True)
+    transactionId = db.Column(db.Integer,db.ForeignKey("transaction.transactionId"))
+    genericType = db.Column(db.String(50))
+    index = db.Column(db.Integer)
+    value = db.Column(db.String(50))
 
